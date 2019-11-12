c61cb609b46a6e895710fcc62774d8207dca0869
Mandarancio <manda.mgf@gmail.com>
2019-11-12 18:02:47 +0100

    working on reports

--- gpt-tests-executer/src/main/resources/report_utils.py
+++ gpt-tests-executer/src/main/resources/report_utils.py
@@ -1,10 +1,11 @@
 """
 Report utils script
 """
-import template
+import template as t
+import sys
 
 
-def create_html_report_per_json(json):
+def create_html_report_per_json(template, json):
     """
 	Create html report for each json test files
 
@@ -12,6 +13,37 @@ def create_html_report_per_json(json):
 	-----------
 	- json: json file
 	"""
+	tmplt = t.Template(template)
+	tmplt.generate(graphTestResult= graphTestResults,
+				   jsonName=json,
+				   operatingSystem=sys.platform,
+				   scope=scope
+				   )
+    # compute start and end date
+            if(graphTestResult.getStartDate() != null) {
+                if (graphTestResult.getStartDate().before(start)) {
+                    start = graphTestResult.getStartDate();
+                }
+            }
+            if(graphTestResult.getEndDate() != null) {
+                if(graphTestResult.getEndDate().after(end)) {
+                    end = graphTestResult.getEndDate();
+                }
+            }
+            totalDuration = totalDuration + graphTestResult.getExecutionTime();
+        }
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        context.put("startDateString", formatter.format(start));
+        context.put("endDateString", formatter.format(end));
+        context.put("totalTime", Math.round((end.getTime()-start.getTime())/1000));
+        context.put("sumTime", totalDuration);
+
+
+        FileWriter fileWriter = new FileWriter(outputPath.toFile());
+        StringWriter writer = new StringWriter();
+        template.merge( context, fileWriter );
+        fileWriter.close();
+
 
 def main():
     """
