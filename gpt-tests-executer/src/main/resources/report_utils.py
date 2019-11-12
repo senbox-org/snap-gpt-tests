ae75d90dd22f5dabb884061b5f6f1b5061086292
Mandarancio <manda.mgf@gmail.com>
2019-11-12 14:06:11 +0100

    better naming

---
+++ gpt-tests-executer/src/main/resources/report_utils.py
@@ -0,0 +1,22 @@
+"""
+Report utils script
+"""
+import template
+
+
+def create_html_report_per_json(json):
+    """
+	Create html report for each json test files
+
+	Parameters:
+	-----------
+	- json: json file
+	"""
+
+def main():
+    """
+    main entry point of the report utils
+    """
+
+if __name__ == '__main__':
+    main()
