da3607771242fb01221fdcefc7d2175630db1801
Martino Ferrari <manda.mgf@gmail.com>
2019-11-14 19:05:47 +0100

    fixed test output

--- gpt-tests-executer/src/main/java/org/esa/snap/test/TestOutput.java
+++ gpt-tests-executer/src/main/java/org/esa/snap/test/TestOutput.java
@@ -1,29 +1,39 @@
 package org.esa.snap.test;
 
+import java.io.File;
 
-public static void main(String[] args) {
-   /* if (args.length != 3) {
-        System.out.println("OUTPUT_PATH EXPECTED_OUTPUT_PATH OUTPUT_NAME");
-        System.exit(1);
-    }
-    String outputNameWithExtension = args[0];
-    String expectedOutput = args[1];
-    String outputName = args[2];
-    Product product = ProductIO.readProduct(outputNameWithExtension);
-    final ExpectedDataset expectedDataset = mapper.readValue(new File(expectedOutput), ExpectedDataset.class);
-    if(product == null){
-        System.out.println("Cannot read output file: " + outputNameWithExtension);
-        System.exit(1);
-    }
-    final ContentAssert contentAssert = new ContentAssert(expectedDataset.getExpectedContent(), outputName, product);
-    try {
-        contentAssert.assertProductContent();
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-    } catch (AssertionError e) {
-        System.out.println("\n\n---------------------------------------------------------------------\n\n");
-        System.out.write("Error when comparing expected output:\n");
-        System.out.println(e.getMessage());
-        System.exit(1);
+import org.esa.snap.core.dataio.ProductIO;
+import org.esa.snap.core.datamodel.Product;
+import org.esa.snap.dataio.ContentAssert;
+import org.esa.snap.dataio.ExpectedDataset;
+
+public class TestOutput {
+    public static void main(String[] args) {
+        if (args.length != 3) {
+            System.out.println("OUTPUT_PATH EXPECTED_OUTPUT_PATH OUTPUT_NAME");
+            System.exit(1);
+        }
+        String outputNameWithExtension = args[0];
+        String expectedOutput = args[1];
+        String outputName = args[2];
+        Product product = ProductIO.readProduct(outputNameWithExtension);
+        final ExpectedDataset expectedDataset = mapper.readValue(new File(expectedOutput), ExpectedDataset.class);
+        if(product == null){
+            System.out.println("Cannot read output file: " + outputNameWithExtension);
+            System.exit(1);
+        }
+        final ContentAssert contentAssert = new ContentAssert(expectedDataset.getExpectedContent(), outputName, product);
+        try {
+            contentAssert.assertProductContent();
+
+        } catch (AssertionError e) {
+            System.out.println("\n\n---------------------------------------------------------------------\n\n");
+            System.out.write("Error when comparing expected output:\n");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+        System.exit(0);
     }
-    System.exit(0);*/
 }
