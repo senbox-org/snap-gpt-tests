f84a615eb469019cbbbaa0172f17bcf296b96c3c
Martino Ferrari <manda.mgf@gmail.com>
2019-11-14 19:34:12 +0100

    fixed typo and removed print

--- gpt-tests-executer/src/main/resources/report_utils.py
+++ gpt-tests-executer/src/main/resources/report_utils.py
@@ -80,9 +80,8 @@ class Test:
         self.start = datetime.datetime.strptime(row[1], __datetime_fmt__)
         self.end = datetime.datetime.strptime(row[2], __datetime_fmt__)
         self.test_set = test_set
-        print('>>>', self.name, self.status)
-            self.json_path = os.path.join('json', test_set, f'{self.name}.json')
-            self.graph_id, self.vm_string = self.__load_json__()
+        self.json_path = os.path.join('json', test_set, f'{self.name}.json')
+        self.graph_id, self.vm_string = self.__load_json__()
         if self.status != 'SKIPPED':
             self.stats = self.__load_perfs__()
         else:
