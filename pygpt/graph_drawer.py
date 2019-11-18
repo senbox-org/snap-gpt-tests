b23144fb5095ab4f0061def9ca295d1f4452230c
Mandarancio <manda.mgf@gmail.com>
2019-11-18 14:57:02 +0100

    Cleaner code

--- pygpt/graph_drawer.py
+++ pygpt/graph_drawer.py
@@ -11,49 +11,67 @@ def __prepare_nodes__(root):
     """
     robust to error graph builder
     """
-    nodes = []
-    nodes_key = {}
-    points = {'x':[], 'y': []}
+    nodes = [] # list of nodes
+    nodes_key = {} # dictionary linking node name to node index
+    points = {'x':[], 'y': []} # point dictionary for geometric info
+
     element = root.xpath('/graph/node')
+    # explore all nodes of the graph
     for elem in element:
-        n_id = elem.get('id')
-        node = {'refs': [], 'name': n_id}
+        n_id = elem.get('id') # get node name
+        node = {'refs': [], 'name': n_id} # create node
         ops = elem.xpath('./operator')
         if ops is not None:
-            node['op'] = ops[0].text
+            node['op'] = ops[0].text # extract operator text
         for src in elem.xpath('./sources/*'):
-            r_id = src.get('refid')
+            # iterate all connected nodes
+            r_id = src.get('refid') # get connected node id
             if r_id is None:
+                # NOTE: in some rare cases the id is inside the node text
                 r_id = src.text
             if r_id not in node['refs']:
+                # add the connected node if needed (avoid multple connections)
                 node['refs'].append(r_id)
-        nodes.append(node)
-        nodes_key[n_id] = len(nodes) - 1
+        nodes.append(node) # add node to list of nodes
+        nodes_key[n_id] = len(nodes) - 1 # connect id with index
 
     element = root.find('.//applicationData[@id="Presentation"]')
-    if element is not None:
-        key = ''
-        index = 0
-        for node in element.iter():
-            if node.tag == 'node':
-                key = node.get('id')
-            if node.tag == 'displayPosition':
-                if key in nodes_key:
-                    ind = nodes_key[key]
-                    nodes[ind]['x'] = float(node.get('x'))
-                    nodes[ind]['y'] = float(node.get('y'))
-                    points['x'].append(nodes[ind]['x'])
-                    points['y'].append(nodes[ind]['y'])
-                elif index < len(nodes) and not 'x' in nodes[index]:
-                    nodes[index]['x'] = float(node.get('x'))
-                    nodes[index]['y'] = float(node.get('y'))
-                    points['x'].append(nodes[index]['x'])
-                    points['y'].append(nodes[index]['y'])
-                index += 1
+    # explore the nodes presentation to respect the correct graphical layout
+    if element is None:
+        return nodes, nodes_key, points
+
+    key = '' # node id
+    index = 0 # node index for workaround of line 58
+    for node in element.iter():
+        # iterate the nodes
+        if node.tag == 'node':
+            key = node.get('id')  # get node id
+        if node.tag == 'displayPosition':
+            # extract geometric info and added it to the node
+            if key in nodes_key:
+                ind = nodes_key[key]
+                nodes[ind]['x'] = float(node.get('x'))
+                nodes[ind]['y'] = float(node.get('y'))
+                points['x'].append(nodes[ind]['x'])
+                points['y'].append(nodes[ind]['y'])
+            elif index < len(nodes) and not 'x' in nodes[index]:
+                """
+                NOTE: this is a workaround for some hand made graph
+                in wich the Presentation node ids are not the same as the
+                real graph nodes and so I use the index of the node instead
+                """
+                nodes[index]['x'] = float(node.get('x'))
+                nodes[index]['y'] = float(node.get('y'))
+                points['x'].append(nodes[index]['x'])
+                points['y'].append(nodes[index]['y'])
+            index += 1
     return nodes, nodes_key, points
 
 
 def __draw_nodes__(axis, nodes, scale):
+    """
+    draw nodes boxes
+    """
     for node in nodes:
         oper = node['op'] if 'op' in node else node['name']
         scaled_x = node['x'] * scale
@@ -65,6 +83,9 @@ def __draw_nodes__(axis, nodes, scale):
 
 
 def __draw_arrows__(axis, nodes, keys):
+    """
+    draw arrows from sources to target nodes
+    """
     for node in nodes:
         ttxt = node['text']
         for ref in node['refs']:
@@ -74,7 +95,7 @@ def __draw_arrows__(axis, nodes, keys):
                 axis.annotate("",
                               xy=(0.0, 0.5), xycoords=ttxt,
                               xytext=(1.0, 0.5), textcoords=stxt,
-                              arrowprops=dict(arrowstyle="->",
+                              arrowprops=dict(arrowstyle="-|>", fc=(0, 0, 0),
                                               connectionstyle="arc3"),
                              )
 
@@ -95,13 +116,15 @@ def draw(source, dest, dpi=90):
         if not points:
             return
 
+        # compute the size of the plot
         real_w = max(points['x']) - min(points['x'])
-        real_h = max(points['y']) - min(points['y']) + 100
+        real_h = max(points['y']) - min(points['y']) + 20
         ratio = real_h / real_w
         scale = 1.0 / real_w
 
-        _, axis = plt.subplots(figsize=(6, 6*ratio), dpi=dpi)
+        _, axis = plt.subplots(figsize=(5, 6*ratio), dpi=dpi)
 
+        # set limits
         plt.xlim([min(points['x']) * scale, max(points['x']) * scale])
         plt.ylim([min(points['y']) * scale - 0.05, max(points['y']) * scale + 0.05])
 
