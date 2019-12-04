c8b6e17d74c0ec67da734bf8f308395a110a5534
Martino Ferrari <manda.mgf@gmail.com>
2019-12-04 20:22:20 +0100

    added auto setters and getters decorators

--- pygpt/core/tools.py
+++ pygpt/core/tools.py
@@ -7,6 +7,8 @@ import datetime
 import sys
 import inspect
 
+from functools import wraps
+
 from core.log import Printable
 
 
@@ -37,3 +39,38 @@ def rlist_files(path, filter_fn=lambda _: True):
         elif filter_fn(file):
             res.append(file)
     return res
+
+
+def auto_getter(func):
+    """
+    Class decorator to automatically genrate all getters for private parameters (if not already defined). 
+    """
+    @wraps(func)
+    def inner(*args, **kwargs):
+        x = func(*args, **kwargs)
+        ctype = type(x)
+        for param in x.__dict__:
+            getter_name = param[2:-2]
+            if param.startswith('__') and param.endswith('__') and not hasattr(ctype, getter_name):
+                def getter(self):
+                    return self.__dict__[param]
+                setattr(ctype, getter_name, getter)
+        return x
+    return inner
+
+def auto_setter(func):
+    """
+    Class decorator to automatically generate all setters for private parameters (if not already defined)
+    """
+    @wraps(func)
+    def inner(*args, **kwargs):
+        x = func(*args, **kwargs)
+        ctype = type(x)
+        for param in x.__dict__:
+            setter_name = f'set_{param[2:-2]}'
+            if param.startswith('__') and param.endswith('__') and not hasattr(ctype, setter_name):
+                def setter(self, value):
+                    self.__dict__[param] = value
+                setattr(ctype, setter_name, setter)
+        return x
+    return inner
