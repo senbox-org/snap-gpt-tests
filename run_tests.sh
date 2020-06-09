e64b762387b4343af3851cd6e91c83c9954af26c
Martino Ferrari <manda.mgf@gmail.com>
2020-06-09 19:00:22 +0200

    script to run locally the GPT test pipeline

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 
+if [[ $# -ne 10 ]]; then
+    echo "Illegal number of parameters"
+    echo "run_tests.sh SNAP_PARENT_DIRECTORY TEST_SCOPE OUTPUT_DIRECTORY MACHINE_TAG"
+    exit 2
+fi
+
 SNAP_DIR=$1
 SCOPE=$2
 ID=`date +%s`
@@ -20,4 +26,4 @@ while read jsonFile; do
     python3 pygpt/snap_gpt_test.py $SNAP_DIR/jre/bin/java "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=${SNAP_DIR}/snap/modules/lib/amd64/libjhdf.so -Dncsa.hdf.hdf5lib.H5.hdf5lib=${SNAP_DIR}/snap/modules/lib/amd64/libjhdf5.so -cp gpt-tests-executer/target/TestOutput.jar" org.esa.snap.test.TestOutput snap.conf $SCOPE $jsonFile $OUTDIR/report false
 done <$OUTDIR/JSONTestFiles.txt
 echo "Updating database"
-python3 pygpt/stats_db.py $DBCONF $TAG ${params.testScope} $OUTDIR/report $ID  LOCAL
+python3 pygpt/stats_db.py $DBCONF $TAG $SCOPE $OUTDIR/report $ID  LOCAL
