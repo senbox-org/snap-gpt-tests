c60c74f4789411c6dbfba1b343859b43b9015702
Martino Ferrari <manda.mgf@gmail.com>
2020-08-10 14:32:02 +0200

    full working pipeline with reports and db update

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-if [[ $# -ne 10 ]]; then
-    echo "Illegal number of parameters"
+if [[ $# -ne 4 ]]; then
+    echo "Illegal number of parameters: ${#}"
     echo "run_tests.sh SNAP_PARENT_DIRECTORY TEST_SCOPE OUTPUT_DIRECTORY MACHINE_TAG"
     exit 2
 fi
@@ -10,22 +10,35 @@ SNAP_DIR=$1
 SCOPE=$2
 ID=`date +%s`
 OUTDIR=$3/$ID
-TAG=$4
-DBCONF=snap-db.conf
+TAG=`hostname`:$4
+DBCONF=conf://snap-db.conf
 
 echo "Working directory: ${OUTDIR}"
+
 mkdir -p $OUTDIR
+mkdir -p $OUTDIR/report
+mkdir -p $OUTDIR/output
+
 echo "Building Output Tester..."
-mvn clean install -DskipTests
+#mvn clean install -DskipTests
 echo "Check test integrity..."
 python3 pygpt/check_jsons.py gpt-tests-resources/tests
 echo "Filtering tests using scope ${SCOPE}"
 python3 pygpt/filter_json.py gpt-tests-resources/tests $SCOPE $OUTDIR
-mkdir $OUTDIR/report
-echo "Running tests...s"
-while read jsonFile; do
+# add empty last line to make sure to execute all tests
+echo "" >> $OUTDIR/JSONTestFiles.txt
+echo "Running tests..."
+cat $OUTDIR/JSONTestFiles.txt | while read jsonFile
+do
     echo "Running ${jsonFile}"
-    python3 pygpt/snap_gpt_test.py $SNAP_DIR/jre/bin/java "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=${SNAP_DIR}/snap/modules/lib/amd64/libjhdf.so -Dncsa.hdf.hdf5lib.H5.hdf5lib=${SNAP_DIR}/snap/modules/lib/amd64/libjhdf5.so -cp gpt-tests-executer/target/TestOutput.jar" org.esa.snap.test.TestOutput snap.conf $SCOPE $jsonFile $OUTDIR/report false
-done <$OUTDIR/JSONTestFiles.txt
+    python3 pygpt/snap_gpt_test.py $SNAP_DIR/jre/bin/java "-Dncsa.hdf.hdflib.HDFLibrary.hdflib=${SNAP_DIR}/snap/modules/lib/amd64/libjhdf.so -Dncsa.hdf.hdf5lib.H5.hdf5lib=${SNAP_DIR}/snap/modules/lib/amd64/libjhdf5.so -cp gpt-tests-executer/target/TestOutput.jar" org.esa.snap.test.TestOutput snap.conf $SCOPE $jsonFile $OUTDIR/output false
+done
+mv $OUTDIR/output $OUTDIR/report/output
+cat $OUTDIR/report/output/Report_*.txt > $OUTDIR/report/output/report.txt
+mv $OUTDIR/report/output/json $OUTDIR/report/ && mv $OUTDIR/report/output/performances $OUTDIR/report/ && mv $OUTDIR/report/output/images $OUTDIR/report/
+cp -R pygpt/statics/* $OUTDIR/report/
+
+echo "Generate report"
+python3 pygpt/report_utils.py pygpt/templates $OUTDIR/report $SCOPE $TAG $DBCONF
 echo "Updating database"
-python3 pygpt/stats_db.py $DBCONF $TAG $SCOPE $OUTDIR/report $ID  LOCAL
+python3 pygpt/stats_db.py $DBCONF $TAG $SCOPE $OUTDIR/report $ID master
