cb8fe1f85da9fa1aa875b166041c62c1a73e6cec
Florian <douziechflorian@gmail.com>
2021-07-20 11:05:32 +0200

    Update test timeout

--- pygpt/snap_gpt_test.py
+++ pygpt/snap_gpt_test.py
@@ -311,7 +311,7 @@ def __run_test__(test, args, properties):
                                        wait=False,
                                        child=False,
                                        plot=True,
-                                       env=enviroment,timeout=2400)
+                                       env=enviroment,timeout=6000)
         # execute the gpt test with the profiler at sampling time 200ms
     else:
         # execute gpt test without profiler
