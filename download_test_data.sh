4c215d93ad1071711066d0920d4ef5f9b95e8d40
BALDO Frederick <frederick.baldo@csgroup.eu>
2023-07-11 09:03:08 +0200

    Fix report generation

--- download_test_data.sh
+++ download_test_data.sh
@@ -15,7 +15,7 @@ S3_ARGS=$5
 while IFS="" read -r line || [ -n "$line" ]
 do
     if [ ! -a "${TEST_DATA_DIR}/${line}" ] && [ ! -d "${TEST_DATA_DIR}/${line}" ]; then
-        if [[ "${line}" =~ (.xml|.XML|.JP2|.zip|.ZIP|.tgz|.NTF|.dim|.DIMA|.h5|.txt|.tif|_OC)$ ]]; then
+        if [[ "${line}" =~ (.xml|.XML|.JP2|.zip|.ZIP|.tgz|.NTF|.dim|.DIMA|.h5|.txt|.tif|_OC|.N1)$ ]]; then
             echo "Download file ${line}"
             aws s3 cp "s3://${S3_BUCKET}/testData/${line}" "${TEST_DATA_DIR}/${line}" $S3_ARGS
         else
