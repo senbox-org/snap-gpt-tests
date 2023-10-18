c7e275936306bd0c9ba16a925857273312d15cc7
BALDO Frederick <frederick.baldo@csgroup.eu>
2023-10-18 11:21:38 +0200

    fix

--- pygpt/stats_db.py
+++ pygpt/stats_db.py
@@ -43,7 +43,6 @@ def __main__():
     try:
         adaptor.open()
         log.info("Database openned")
-        log.debug(args)
         test_sets = ru.get_test_sets(args.base_path)
         tag_id = adaptor.docker_tag_id(args.tag_name)
         adaptor.create_job_entry(args.job, args.branch, args.test_scope, tag_id, test_sets)
