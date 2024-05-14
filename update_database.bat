906e634c7655b7920931d6dc90dd60d80b8f2c8d
BALDO Frederick <frederick.baldo@csgroup.eu>
2024-05-14 12:47:38 +0200

    fix(windows): Update database after pipeline run

--- update_database.bat
+++ update_database.bat
@@ -3,14 +3,15 @@ set CI_PROJECT_DIR=%CI_PROJECT_DIR%
 set CI_JOB_ID=%CI_JOB_ID%
 set DB_PATH=%DB_PATH%
 set SCOPE=%SCOPE%
+set JOB=%CI_JOB_ID:~6,10%
 IF %CI_COMMIT_BRANCH%==master IF %SCOPE%==daily (
-    ::Get next job id (only works for master)
-    ::curl https://reports-api.snap-ci.ovh/api/job/last | %APPDATA%\jq.exe .ID >> %CI_PROJECT_DIR%\jobid.txt
-    ::set /p JOB=<%CI_PROJECT_DIR%\jobid.txt
-    ::set /a JOB=%JOB%+1
-    ::echo "Saving results for job %JOB%"
-    ::Update reports database
-    python %CI_PROJECT_DIR%\pygpt\stats_db.py %DB_PATH% "snap:master" %SCOPE% %CI_PROJECT_DIR%\result\report %CI_JOB_ID:6,10% master
-    DEL %CI_PROJECT_DIR%\jobid.txt
+    @REM ::Get next job id (only works for master)
+    @REM ::curl https://reports-api.snap-ci.ovh/api/job/last | %APPDATA%\jq.exe .ID >> %CI_PROJECT_DIR%\jobid.txt
+    @REM ::set /p JOB=<%CI_PROJECT_DIR%\jobid.txt
+    @REM ::set /a JOB=%JOB%+1
+    @REM ::echo "Saving results for job %JOB%"
+    @REM ::Update reports database
+    python %CI_PROJECT_DIR%\pygpt\stats_db.py %DB_PATH% "snap:master" %SCOPE% %CI_PROJECT_DIR%\result\report %JOB% master
+    @REM DEL %CI_PROJECT_DIR%\jobid.txt
     echo "done"
 )
