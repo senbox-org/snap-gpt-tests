f8f7454307237949e49e025a446666b518c62180
Alexandru ILIOIU <alexandru.ilioiu@c-s.ro>
2024-11-08 15:05:53 +0200

    test(ci): check MD5 of snap installer (6)

--- checkMD5.bat
+++ checkMD5.bat
@@ -2,12 +2,12 @@
 
 setlocal enabledelayedexpansion
 REM Retrieve (from Nexus) MD5 checksum of SNAP installer executable file and set into variable
-for /F "usebacq tokens=*" %%a in (`curl "%NEXUS_URL%/%SNAP_INSTALLER_EXE%.md5"`) do (
+for /F "usebackq tokens=*" %%a in (`curl "%NEXUS_URL%/%SNAP_INSTALLER_EXE%.md5"`) do (
     SET "SNAP_INSTALLER_MD5=%%a"
 )
 echo SNAP_INSTALLER_MD5 = %SNAP_INSTALLER_MD5%
 REM Compute MD5 checksum of downloaded SNAP installer executable
-for /F "usebacq skip=1 eol=C tokens=1" %%a in (`certutil -hashfile "%DL_DIR%\%SNAP_INSTALLER_EXE%" MD5`) do (
+for /F "usebackq skip=1 eol=C tokens=1" %%a in (`certutil -hashfile "%DL_DIR%\%SNAP_INSTALLER_EXE%" MD5`) do (
     SET "LOCAL_MD5=%%a"
 )
 echo LOCAL_MD5 = %LOCAL_MD5%
