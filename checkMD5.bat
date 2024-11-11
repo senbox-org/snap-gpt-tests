2ed0153b5acd297ebf6d8df2cb932e774caf81c5
Alexandru ILIOIU <alexandru.ilioiu@c-s.ro>
2024-11-11 17:33:19 +0200

    test(ci): check MD5 of snap installer (27)

--- checkMD5.bat
+++ checkMD5.bat
@@ -1,5 +1,6 @@
 setlocal enabledelayedexpansion
 REM Retrieve (from Nexus) MD5 checksum of SNAP installer executable file and set into variable
+SET "localReturnValue=3"
 FOR /F "usebackq tokens=*" %%a IN (`curl "%NEXUS_URL%/%SNAP_INSTALLER_EXE%.md5"`) do (
     SET "SNAP_INSTALLER_MD5=%%a"
 )
@@ -13,12 +14,14 @@ IF /I %SNAP_INSTALLER_MD5%==%LOCAL_MD5% (
     echo "MD5 checksum ok - downloaded file 'SNAP installer' (%SNAP_INSTALLER_EXE%) is ok."
     endlocal
     :: exit with ERRORLEVEL = 0
-    EXIT /B 2
+    REM EXIT /B
+    SET "localReturnValue=1"
 ) ELSE (
     echo "MD5 checksum error - downloaded file 'SNAP installer' (%SNAP_INSTALLER_EXE%) is corrupt."
     echo "ERROR - exit from job / pipeline !"
     endlocal
     :: exit with ERRORLEVEL > 0
-    EXIT /B 3
+    REM EXIT /B 3
+    SET "localReturnValue=3"
 ) 
-endlocal 
+endlocal & SET "md5RetVal=%localReturnValue%"
