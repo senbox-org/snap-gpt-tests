874fe02fb65c86eedc576b7983d6c7b2d80bb5c1
Alexandru ILIOIU <alexandru.ilioiu@c-s.ro>
2025-01-17 15:26:32 +0200

    update(ci): add variable DISPLAY_VERSION

--- update_database.bat
+++ update_database.bat
@@ -3,15 +3,16 @@ set CI_PROJECT_DIR=%CI_PROJECT_DIR%
 set CI_JOB_ID=%CI_JOB_ID%
 set DB_PATH=%DB_PATH%
 set SCOPE=%SCOPE%
+:: compute job number for performance reports
 set JOB=%CI_JOB_ID:~6,10%
-IF %CI_COMMIT_BRANCH%==master IF %SCOPE%==daily (
+IF %SCOPE%==daily (
     @REM example (command executed on Windows VM):
-    @REM python %CI_PROJECT_DIR%\pygpt\stats_db.py %DB_PATH% <report_group_name> %SCOPE% <base_path> %JOB% <branch>
+    @REM python %CI_PROJECT_DIR%\pygpt\stats_db.py %DB_PATH% <display_version> %SCOPE% <base_path> %JOB% <branch>
     @REM   CI_PROJECT_DIR = C:\builds\TPp69-DL\0\senbox-org\snap-gpt-tests\
     @REM   DB_PATH = mysql://<user:passwd>@mysql.snap-ci.ovh:3306/snap_reports_2024
     @REM                    (defined as variable into 'snap-gpt-tests' project from Gitlab)
-    @REM   report_group_name = snap:9.0.8-RC2 (with this (branch or tag) name will be saved into performance database on https://reports.snap-ci.ovh/?#/branches)
-    @REM                       (it contains always the prefix snap:)
+    @REM   display_version = snap:9.0.8-RC2 (with this (branch or tag) name will be saved into performance database on https://reports.snap-ci.ovh/?#/branches)
+    @REM                       (it always contains the prefix 'snap:')
     @REM   SCOPE = daily (or CItest for pipeline test purposes)
     @REM   base_path = %CI_PROJECT_DIR%\result\report
     @REM   JOB = 126 
