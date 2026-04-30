ca2f2b3996bb02384ef98acfd93b03ac1b8cd6cd
Daniel Nistor <daniel.nistor@c-s.ro>
2026-04-30 11:18:17 +0300

    update(ci): fixing error 1 in cleaning

--- run_tests.bat
+++ run_tests.bat
@@ -13,5 +13,7 @@ FOR /F "usebackq tokens=1,2* delims=" %%t in ("%CI_PROJECT_DIR%\%REPORT_DIR%\JSO
     echo  java args: "%JAVA_OPTIONS% -cp %CLASSPATH%"
     echo %USERPROFILE%
     python "%CI_PROJECT_DIR%\pygpt\snap_gpt_test.py" "%JAVA_HOME%\bin\java.exe" "-cp %CLASSPATH%" org.esa.snap.test.TestOutput "%CI_PROJECT_DIR%\%PROPERTIES_PATH%" %SCOPE% "%CI_PROJECT_DIR%\%%t" "%CI_PROJECT_DIR%\%REPORT_DIR%\report\output" true
+    GOTO :end_loop
 )
+:end_loop
 endlocal
